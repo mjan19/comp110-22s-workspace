ab3f9c04b4da90395572ac80b779f3d996ae1144
Jan Hernandez <jhernandez1@unc.edu>
2022-01-24 16:26:16 -0500

    Exercise 1 Completed

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,37 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730510654"
+
+number_of_matches = 0
+five_character_word: str = input("Enter a 5-character word:")
+if len(five_character_word) == 5:
+    single_character: str = input("Enter a single character:")
+    if len(single_character) == 1:
+        print("Searching for " + single_character + " in " + five_character_word)
+        if single_character == five_character_word[0]:
+            print(single_character + " found at index 0")
+            number_of_matches = number_of_matches + 1
+        if single_character == five_character_word[1]:
+            print(single_character + " found at index 1")
+            number_of_matches = number_of_matches + 1
+        if single_character == five_character_word[2]:
+            print(single_character + " found at index 2")
+            number_of_matches = number_of_matches + 1
+        if single_character == five_character_word[3]:
+            print(single_character + " found at index 3")
+            number_of_matches = number_of_matches + 1
+        if single_character == five_character_word[4]:
+            print(single_character + " found at index 4")
+            number_of_matches = number_of_matches + 1
+        if number_of_matches == 0:
+            print("No instances of " + single_character + " found in " + five_character_word)
+        else: 
+            if number_of_matches == 1:
+                print("1 instance of " + single_character + " found in " + five_character_word)
+            else:
+                if number_of_matches > 1:
+                    print(str(number_of_matches) + " instances of " + single_character + " found in " + five_character_word)
+    else: 
+        print("Error: Character must be a single character.")
+else:
+    print("Error: Word must contain 5 characters")
