3a73016fe6835c3d3b1ac801a1bb2554851bb27a
Jan Hernandez <jhernandez1@unc.edu>
2022-02-23 03:25:03 -0500

    Learned about dictionaries

---
+++ lessons/dictionaries.py
@@ -0,0 +1,19 @@
+"""Demonstrations of dictionary capabilities."""
+
+
+schools: dict[str, int]
+
+schools = dict()
+
+schools["UNC"] = 19_400
+schools["Duke"] = 6_717
+schools["NCSU"] = 26_150
+print(schools)
+print(f"UNC has {schools['UNC']} students.")
+
+
+schools.pop("Duke")
+
+
+is_duke_present: bool = "Duke" in schools
+print(f"Duke is present: {is_duke_present}")
